98e71a30c16c9bf974122c86a11c64295d995469
monicabatallanes <mbbatallanes1@gmail.com>
2022-09-01 18:14:25 -0300

    modifiqué navbar

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Navbar, Container, Nav} from 'react-bootstrap';
-import '../Images/EgreMax-marca-Rojo-03.jpg';
+import logo from '../Images/EgreMax-marca-Rojo-03.jpg';
+import './navbar.css';
 
 const BarraNav = () => {
     return (
         
         <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Container className='container-nav'>
+          <Navbar.Brand href="#home">
+            <img className='logo' src={logo} alt='logoEgremax'/>
+          </Navbar.Brand>
+          
+          <Nav >
+            <Nav.Link className='links' href="#inicio">Inicio</Nav.Link>
+            <Nav.Link className='links' href="#conocenos">Conocenos</Nav.Link>
+            <Nav.Link className='links' href="#servicios">Servicios</Nav.Link>
+            <Nav.Link className='links' href="#contacto">Contacto</Nav.Link>
           </Nav>
+          
+          
         </Container>
       </Navbar>
         
